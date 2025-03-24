7bd6bcf066504d36ad04a2c0a163ba4e7d6a0f73
halfmin <halfmin@gmail.com>
2025-03-24 10:10:21 +0900

    AritcleRepository 생성

---
+++ ArticleApp.Models/Articles/ArticleRepository.cs
@@ -0,0 +1,40 @@
+﻿using Dul.Domain.Common;
+
+namespace ArticleApp.Models
+{
+    /// <summary>
+    /// Repository Class: ADO.NET or Dapper or Entity Framework Core 
+    /// </summary>
+    public class ArticleRepository : IArticleRepository
+    {
+        public Task<Article> AddArticleAsync(Article model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteArticleAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Article> EditArticleAsync(Article model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagingResult<Article>> GetAllAsync(int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Article> GetArticleByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Article>> GetArticlesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
-#
+#    
+
+https://www.youtube.com/playlist?list=PLO56HZSjrPTC1c1MbY72vVT0sO33G-9Od
+git : https://github.com/VisualAcademy/articleapp
