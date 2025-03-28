f546d4ffb1ea5cef2747e79a95f499b1b6946e86
halfmin <halfmin@gmail.com>
2025-03-28 11:07:47 +0900

    인라인=> 코드비하인드 로 변경

---
+++ ArticleAppBlazorServer/Pages/Articles/Create.razor.cs
@@ -0,0 +1,44 @@
+﻿using ArticleApp.Models;
+using Microsoft.AspNetCore.Components;
+
+
+namespace ArticleAppBlazorServer.Pages.Articles
+{
+    public partial class Create
+    {
+        [Inject]
+        private ILogger<Create> Logger { get; set; }
+
+        [Inject]
+        private IArticleRepository ArticleRepository { get; set; }
+
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+
+        private string? errorMessage;
+
+        [SupplyParameterFromForm]
+        public Article Model { get; set; } = new Article();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Logger.LogInformation("※※※[1]Create 폼 시작");
+        }
+
+        protected async Task btnSubmit_Click()
+        {
+            Logger.LogInformation("※※※[2]Create Submit");
+            try
+            {
+                await ArticleRepository.AddArticleAsync(Model);
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                Logger.LogError(ex, "Create 오류");
+            }
+            //리스트 페이지로 이동
+            NavigationManager.NavigateTo("/Articles");
+        }
+    }
+}
---
+++ ArticleAppBlazorServer/Pages/Articles/Index.razor.cs
@@ -0,0 +1,82 @@
+﻿using ArticleApp.Models;
+using Dul.Domain.Common;
+using Dul.Web;
+using Microsoft.AspNetCore.Components;
+
+namespace ArticleAppBlazorServer.Pages.Articles
+{
+    public partial class Index
+    {
+        [Inject]
+        private ILogger<Create> Logger { get; set; }
+
+        [Inject]
+        private IArticleRepository ArticleRepository { get; set; }
+
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+
+        private List<Article> articles;
+
+        // 페이저 객체 생성
+        private PagerBase pager = new PagerBase()
+        {
+            PageNumber = 1,
+            PageIndex = 0,
+            PageSize = 3,
+            PagerButtonCount = 5
+        };
+
+        /// <summary>
+        /// 페이지 로드  
+        /// </summary>
+        /// <returns></returns>
+        protected override async Task OnInitializedAsync()
+        {
+            Logger.LogInformation("※※※ [1]Index 시작");
+
+            //[1] 전체 데이터 출력
+            //articles = await ArticleRepository.GetArticlesAsync();
+
+            //[2] 페이징 처리
+            try
+            {
+                PagingResult<Article> pagingData = await ArticleRepository.GetAllAsync(pager.PageIndex, pager.PageSize);
+                pager.RecordCount = pagingData.TotalRecords; // 전체 레코드 수
+                Logger.LogInformation($"※※※ [2] 전체 레코드 수 {pager.RecordCount} , {pager.PageNumber}페이지");
+                articles = pagingData.Records.ToList(); // 현재 페이지의 레코드
+
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "index 오류");
+            }
+
+        }
+
+        //pager버튼 클릭 콜백 함수
+
+        private async void PageIndexChangedIndex(int pageIndex)
+        {
+            Logger.LogInformation($"※※※ [3] PageIndexChanged 호출 pageIndex={pageIndex}");
+            pager.PageIndex = pageIndex;
+            pager.PageNumber = pageIndex + 1;
+
+            PagingResult<Article> pagingData = await ArticleRepository.GetAllAsync(pager.PageIndex, pager.PageSize);
+            pager.RecordCount = pagingData.TotalRecords; // 전체 레코드 수
+            articles = pagingData.Records.ToList(); // 현재 페이지의 레코드
+
+            StateHasChanged(); //현재 페이지를 다시 그림
+        }
+
+        /// <summary>
+        ///  제목의 td 클릭시 상세 페이지로 이동
+        /// </summary>
+        /// <param name="id"></param>
+        private void btnTitle_Click(int id)
+        {
+            Logger.LogInformation($"※※※ [4] btnTitle_Click 호출 id={id}");
+            NavigationManager.NavigateTo($"/Articles/Details/{id}");
+        }
+    }
+}
