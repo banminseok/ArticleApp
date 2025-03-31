24405f48f38f090a626f5e86ae0c6f9b2400369b
halfmin <halfmin@gmail.com>
2025-03-31 09:49:07 +0900

    리포지토리 인터페이스 클래스 기본 모양 구현하기

---
+++ ArticleApp.Models/Notices/INoticeRepositoryAsync.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticleApp.Models
+{
+
+
+    /// <summary>
+    /// [4] Repository Interface
+    /// </summary>
+    public interface INoticeRepositoryAsync: ICrudRepositoryAsync<Notice>
+    {
+        
+    }
+}
---
+++ ArticleApp.Models/Notices/NoticeRepositoryAsync.cs
@@ -0,0 +1,45 @@
+﻿using Dul.Domain.Common;
+
+namespace ArticleApp.Models
+{
+    /// <summary>
+    /// [6] Repository Class: ADO.NET or Dapper or Entity Framework Core
+    /// </summary>
+    public class NoticeRepositoryAsync : INoticeRepositoryAsync
+    {
+        public Task<Notice> AddAsync(Notice model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EditAsync(Notice model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Notice>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagingResult<Notice>> GetAllAsync(int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagingResult<Notice>> GetAllByParentIdAsync(int pageIndex, int pageSize, int parentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Notice> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
