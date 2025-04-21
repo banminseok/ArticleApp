8b89236de50aa8eb8aff356bd04a2d0c7c5f2a8e
halfmin <halfmin@gmail.com>
2025-04-21 10:45:38 +0900

    19_03_component 태그 헬퍼를 사용하여 CSHTML 페이지에 Blazor 컴포넌트 포함하여 실행시키기

---
+++ ArticleAppBlazorServer/Controllers/ComponentTestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArticleAppBlazorServer.Controllers
+{
+    public class ComponentTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
