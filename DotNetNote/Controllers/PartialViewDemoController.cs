821f4226afaaa8d4ee0ef9e5dab363beb174e254
halfmin <halfmin@gmail.com>
2025-05-20 16:30:18 +0900

    29_01_MVC_AdvancedPaging_ASP.NET Core 공통 페이징 컨트롤 만들기

---
+++ DotNetNote/Controllers/PartialViewDemoController.cs
@@ -0,0 +1,22 @@
+﻿using Dul.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers
+{
+    public class PartialViewDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var pager = new PagerBase
+            {
+                PageIndex = 1,
+                PageCount = 3,
+                PageSize = 10,
+                RecordCount = 100,
+                Url = "PartialViewDemo",
+            };
+            ViewBag.PageModel = pager;
+            return View();
+        }
+    }
+}
