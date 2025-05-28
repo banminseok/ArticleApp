8867597cb290c779f17674e929b62e66e4716264
halfmin <halfmin@gmail.com>
2025-05-28 15:08:35 +0900

    MVC 뷰 컴포넌트를 사용하여 Carousel 기능을 메인 페이지에 포함하기

---
+++ DotNetNote/ViewComponents/AzCarouselViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.ViewComponents
+{
+    public class AzCarouselViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            // ViewBag.CopyRight = $"Copyright © {DateTime.Now.Year}. All rights reserved.";
+            // return View("Default");
+            return View();
+        }
+    }
+}
