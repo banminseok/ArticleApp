202d5a5ca7a95dabf10159db9671796aaf56bd21
halfmin <halfmin@gmail.com>
2025-06-09 14:13:59 +0900

    37_26_NoteCommentServiceController_최근 댓글 리스트 5개 출력하는 Web API 만들기

---
+++ DotNetNote/Controllers/DotNetNote/NoteCommentServiceController.cs
@@ -0,0 +1,23 @@
+﻿using DotNetNote.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers.DotNetNote
+{
+    /// <summary>
+    /// 최근 댓글 리스트를 반환하는 컨트롤러
+    /// •	C# 12 이상에서는 Primary Constructor 문법
+    /// </summary>
+    /// <param name="repository"></param>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteCommentServiceController(INoteCommentRepository repository) : ControllerBase
+    {
+        public IEnumerable<NoteComment> Get()
+        {
+            // 최근 댓글 리스트 반환
+            return repository.GetRecentComments();      // 캐싱 적용 전
+            //return repository.GetRecentCommentsNoCache();   // 캐싱 적용 후
+        }
+    }
+}
