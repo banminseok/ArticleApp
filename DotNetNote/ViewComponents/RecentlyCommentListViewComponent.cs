50e17a045e33ad7f2b3a131fd3005de5e28b373b
halfmin <halfmin@gmail.com>
2025-06-09 11:18:37 +0900

    37_24_RecentlyCommentListViewComponent_최근 댓글 리스트 5개 출력하는 뷰 컴포넌트 만들기

---
+++ DotNetNote/ViewComponents/RecentlyCommentListViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using DotNetNote.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.ViewComponents
+{
+    /// <summary>
+    /// 최근 댓글 리스트
+    /// Views>Shared 폴더에 Components 폴더를 만들고, 그 안에 RecentlyCommentList 폴더 생성 , Default.cshtml 파일 생성
+    /// </summary>
+    public class RecentlyCommentListViewComponent : ViewComponent
+    {
+        // 댓글 리포지토리 개체
+        private INoteCommentRepository _repository;
+
+        public RecentlyCommentListViewComponent(
+            INoteCommentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // 최근 댓글 리스트 전달
+        //public IViewComponentResult Invoke() => View(_repository.GetRecentComments());
+        public IViewComponentResult Invoke()
+        {
+            // 최근 댓글 리스트를 가져와서 뷰에 전달
+            var recentComments = _repository.GetRecentComments();
+            return View(recentComments);
+        }
+    }
+}
