da8b633b85d3f8f942c9c0745d3fe5bd250513b7
halfmin <halfmin@gmail.com>
2025-06-09 15:15:19 +0900

    37_29_참고_DotNetNote_Database 프로젝트에 Boards 테이블과 BoardViews 뷰 만들기, 37_30_참고_Board 모델 클래스_BoardViews(Boards 테이블) 뷰와 일대일로 매핑되는 모델 클래스 생성_게시판 강의 종료

---
+++ DotNetNote/Controllers/Board/BoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers.Board
+{
+    public class BoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
