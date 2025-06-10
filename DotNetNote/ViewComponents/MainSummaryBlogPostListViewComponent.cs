1fe367545bd06af51f5f5579ff9de5591120b126
halfmin <halfmin@gmail.com>
2025-06-10 10:43:46 +0900

    38_05_참고_MainSummaryBlogPostList_메인에 블로그 리스트 출력하는 뷰 컴포넌트 적용하기

---
+++ DotNetNote/ViewComponents/MainSummaryBlogPostListViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using DotNetNote.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.ViewComponents
+{
+    public class MainSummaryBlogPostListViewComponent(INoteRepository repository) : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var model = repository.GetNoteSummaryByCategoryBlog("Blog");
+            return View(model);
+        }
+    }
+}
