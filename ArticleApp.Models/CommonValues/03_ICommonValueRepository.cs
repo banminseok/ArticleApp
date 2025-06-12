e7ff11502df414bc00b35044ad4680cfcac7c331
halfmin <halfmin@gmail.com>
2025-06-12 14:58:33 +0900

    02_02_라이브 코딩_1_CommonValues 모듈 작성 시작 Models 프로젝트까지 1차 구성

---
+++ ArticleApp.Models/CommonValues/03_ICommonValueRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticleApp.Models
+{
+    /// <summary>
+    /// [3] Repository Interface
+    /// </summary>
+    public interface ICommonValueRepository : ICommonValueCrudRepository<CommonValue>
+    {
+        Task<Tuple<int, int>> GetStatus(int parentId);
+        Task<bool> DeleteAllByParentId(int parentId);
+        Task<SortedList<int, double>> GetMonthlyCreateCountAsync();
+
+        Task<string> FindValueByTest(string text);
+    }
+}
