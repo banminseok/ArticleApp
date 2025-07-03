0db1de73df27e30c4d40c0ead8f12e6e32fb599e
halfmin <halfmin@gmail.com>
2025-07-03 15:37:37 +0900

    09_02_Blazor Server에서 쿠키 인증을 사용하여 로그아웃 처리하기

---
+++ Hawaso.Cookies/Controllers/LogoutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hawaso.Cookies.Controllers
+{
+    public class LogoutController : Controller
+    {
+        //public async Task<IActionResult> Index()
+        //{
+        //    // [!] 인증 해제: 쿠키를 삭제하여 인증 정보를 제거
+        //    await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        //    return LocalRedirect("/");
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> Index()
+        {
+            // [!] 인증 해제: 쿠키를 삭제하여 인증 정보를 제거
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return LocalRedirect("/");
+        }
+    }
+}
